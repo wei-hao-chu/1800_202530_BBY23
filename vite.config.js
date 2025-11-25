4df5576a516f0d6ac1d62bc3f92cdb321480ea3e
Matthew Vasquez <172213327+Gamecoder3D@users.noreply.github.com>
2025-11-25 15:04:43 -0800

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,13 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
+        // If you add new HTML pages, add them here.
         index: resolve(__dirname, "index.html"),
         login: resolve(__dirname, "login.html"),
         main: resolve(__dirname, "main.html"),
-        // We dont have a profile page yet but this is here for in the future.
-        profile: resolve(__dirname, "profile.html"),
+        // We dont have a profile page yet but this is here just in case
+        // for in the future, just uncomment it if you do ever add one.
+        // profile: resolve(__dirname, "profile.html"),
         goals: resolve(__dirname, "goals.html"),
         quiz: resolve(__dirname, "quiz.html"),
         // Don't put template.html here since user shouldn't even need to access it.
